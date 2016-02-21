37b7738c3ccc85379e18c04575695d85c9bd078a
koba-e964 <koba_max@kobas-Air.pohe>
2016-02-22 02:57:59 +0900

    Fix llIntern in SKICompiler.scala

--- scala/LambdaTerm.scala
+++ scala/LambdaTerm.scala
@@ -1,20 +1,24 @@
 sealed abstract class LambdaTerm {
   def toString: String
   def toStringParen: String = "(" + toString + ")"
+  def freeVars: Set[String] = Set()
 }
 
 case class LambdaAbst(v: String, e: LambdaTerm) extends LambdaTerm {
   override def toString = "\\" + v + ". " + e.toString
+  override def freeVars = e.freeVars - v
 }
 /*
  * TODO Left-associativity of function application is not implemented. (toString yields redundant parentheses)
  */
 case class LambdaApp(e1: LambdaTerm, e2: LambdaTerm) extends LambdaTerm {
   override def toString = e1.toStringParen + " " + e2.toStringParen
+  override def freeVars = e1.freeVars ++ e2.freeVars
 }
 case class LambdaVar(v: String) extends LambdaTerm {
   override def toString = v
   override def toStringParen = v
+  override def freeVars = Set(v)
 }
 
 /*
--- scala/SKICompiler.scala
+++ scala/SKICompiler.scala
@@ -10,19 +10,22 @@ class SKICompiler {
     cnt += 1
     "G" + cnt
   }
-  type Env = Map[String, (String, IR)]
-  type MuEnv = mu.Map[String, (String, IR)]
+  type Env = Map[String, (List[String], IR)]
+  type MuEnv = mu.Map[String, (List[String], IR)]
   sealed abstract class IR {
-    
+    def toString: String
+    def toStringParen: String = toString
   }
-  case object IRS extends IR
-  case object IRK extends IR
-  case object IRI extends IR
   case class IRApp(e1: IR, e2: IR) extends IR {
-    
+    override def toString = e1.toString + " " + e2.toStringParen
+    override def toStringParen = "(" + toString + ")"
   }
-  case class IRVar(name: String) extends IR
-  case class IRGlobal(name: String) extends IR
+  case class IRVar(name: String) extends IR {
+    override def toString = name
+  }
+  case class IRGlobal(name: String) extends IR {
+    override def toString = name
+  }
   
   def lambdaLift(e: LambdaTerm): (IR, Env) = {
     cnt = 0
@@ -30,16 +33,27 @@ class SKICompiler {
     val lli = llIntern(e)
     (lli, env.toMap)
   }
+  def convertToSKI(ir: IR, env: Env): SKI.Expr = ir match {
+    case IRGlobal(name) =>
+      val (u, v) = env(name)
+      convInternal(u, v, env)
+    case IRApp(i1, i2) => SKI.EApp(convertToSKI(i1, env), convertToSKI(i2, env))
+    case IRVar(x) => scala.sys.error("given ir is not closed!!")
+  }
   def llIntern(e: LambdaTerm): IR = e match {
     case LambdaAbst(x, t) => {
       /* add env */
       val irt = llIntern(t)
       val name = freshName
-      env += name -> (x -> irt)
-      IRGlobal(name)
+      val freeVars: List[String] = (t.freeVars - x).toList
+      env += name -> ((freeVars :+ x) -> irt)
+      freeVars.foldLeft(IRGlobal(name): IR)((x, y) => IRApp(x, IRVar(y)))
     }
     case LambdaApp(t1, t2) => IRApp(llIntern(t1), llIntern(t2))
     case LambdaVar(x) => IRVar(x)
+  }
+  def convInternal(args: List[String], e: IR, env: Env): SKI.Expr = {
+    scala.sys.error("convInternal")
   }
 }
 
