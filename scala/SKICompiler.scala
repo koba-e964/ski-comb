13dc7bc730a01a42ded7f2d3f1cd657825ec6e43
koba-e964 <koba_max@kobas-Air.pohe>
2016-02-23 23:38:30 +0900

    Eliminate all tabs in SKICompiler.scala

--- scala/SKICompiler.scala
+++ scala/SKICompiler.scala
@@ -45,58 +45,58 @@ object SKICompiler {
   }
   private[this] final class LLConverter {
     type MuEnv = mu.Map[String, (List[String], IR)]
-	  var cnt: Int = 0 // counter
-	  val env: MuEnv = mu.Map()
-	  private[this] def freshName: String = {
-			cnt += 1
-			"G" + cnt
-	  }
-	  def llIntern(e: LambdaTerm): IR = e match {
-	  case LambdaAbst(x, t) => {
-		  /* add env */
-		  val irt = llIntern(t)
-		  val name = freshName
-		  val freeVars: List[String] = (t.freeVars - x).toList
-		  env += name -> ((freeVars :+ x) -> irt)
-		  freeVars.foldLeft(IRGlobal(name): IR)((x, y) => IRApp(x, IRVar(y)))
-	  }
-	  case LambdaApp(t1, t2) => IRApp(llIntern(t1), llIntern(t2))
-	  case LambdaVar(x) => IRVar(x)
-	  }
+    var cnt: Int = 0 // counter
+    val env: MuEnv = mu.Map()
+    private[this] def freshName: String = {
+      cnt += 1
+      "G" + cnt
+    }
+    def llIntern(e: LambdaTerm): IR = e match {
+      case LambdaAbst(x, t) => {
+      /* add env */
+      val irt = llIntern(t)
+      val name = freshName
+      val freeVars: List[String] = (t.freeVars - x).toList
+      env += name -> ((freeVars :+ x) -> irt)
+      freeVars.foldLeft(IRGlobal(name): IR)((x, y) => IRApp(x, IRVar(y)))
+      }
+      case LambdaApp(t1, t2) => IRApp(llIntern(t1), llIntern(t2))
+      case LambdaVar(x) => IRVar(x)
+    }
   }
   /* TODO memoization */
   private[this] class IRConverter(env: Env) {
-	  def convInternal(args: List[String], e: IR): IR = {
-			val et = elimGlobal(e)
-			if (args.isEmpty) et else convInternal(args.init, convInternalOne(args.last, et))
-	  }
-	  val s = IRGlobal("S")
-	  val k = IRGlobal("K")
-	  val i = IRGlobal("I")
+    def convInternal(args: List[String], e: IR): IR = {
+      val et = elimGlobal(e)
+      if (args.isEmpty) et else convInternal(args.init, convInternalOne(args.last, et))
+    }
+    val s = IRGlobal("S")
+    val k = IRGlobal("K")
+    val i = IRGlobal("I")
     def convInternalOne(arg: String, e: IR): IR = e match {
       case _ if (e.freeVars(arg) == false) => IRApp(k, e) // optimization
       case IRApp(e1, IRVar(x)) if (x == arg && e1.freeVars(arg) == false) =>
       e1 // optimization, not necessary
-  	  case IRApp(e1, e2) => IRApp(IRApp(s, convInternalOne(arg, e1)), convInternalOne(arg, e2))
-  	  case IRVar(x) => if (x == arg) i else IRApp(k, IRVar(x))
-  	  case IRGlobal(g) => g match {
-    	  case "S" => IRApp(k, s)
-    	  case "K" => IRApp(k, k)
-    	  case "I" => IRApp(k, i)
-    	  case _ => scala.sys.error("global combinator, which is not expected")
-  		}
-	  }
-	  def elimGlobal(e: IR): IR = e match {
-  	  case IRGlobal(g) => g match {
-    	  case "S" => e
-	      case "K" => e
-	      case "I" => e
-	      case _ => val (a, t) = env(g)
-	      convInternal(a, t)
-	    }
+      case IRApp(e1, e2) => IRApp(IRApp(s, convInternalOne(arg, e1)), convInternalOne(arg, e2))
+      case IRVar(x) => if (x == arg) i else IRApp(k, IRVar(x))
+      case IRGlobal(g) => g match {
+        case "S" => IRApp(k, s)
+        case "K" => IRApp(k, k)
+        case "I" => IRApp(k, i)
+        case _ => scala.sys.error("global combinator, which is not expected")
+      }
+    }
+    def elimGlobal(e: IR): IR = e match {
+      case IRGlobal(g) => g match {
+        case "S" => e
+        case "K" => e
+        case "I" => e
+        case _ => val (a, t) = env(g)
+        convInternal(a, t)
+      }
       case IRApp(t1, t2) => IRApp(elimGlobal(t1), elimGlobal(t2))
       case _ => e
-	  }
+    }
   }
 }
 
